33db2fb2b5fd2c12fa9863c9bad6d7d3ce25cea3
shpark <shpark5677@gmail.com>
2017-02-02 12:23:13 +0900

    'Git.java'

---
+++ Git.java
@@ -0,0 +1,12 @@
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("Hello java");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+        
+    }
+}
