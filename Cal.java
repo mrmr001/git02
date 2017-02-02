e7c25f19a05b8d5969f32294a86bbf1cff090356
shpark <shpark5677@gmail.com>
2017-02-02 16:38:43 +0900

    static change

--- Cal.java
+++ Cal.java
@@ -1,12 +1,16 @@
 public class Cal{
    
-	public int add(int x, int y) {
+	public static int add(int x, int y) {
 	return x+y;
 	}
-	public int substrat(int x, int y) {
+	public static int substrat(int x, int y) {
 	return x-y;
-	
-	
+	}
+	public static int mul(int x, int y) {
+	return x*y;
+	}
+	public static int divide(int x, int y) {
+	return x/y;
         
     }
 }
--- Git.java
+++ Git.java
@@ -8,10 +8,13 @@ public class Git {
         System.out.println("*****");
         System.out.println("******");
 
-	Cal c = new Cal();
+	
 
-System.out.println("3+4="+c.add(3,4));
-System.out.println("3-4="+c.substrat(3,4));
+System.out.println("3+4="+Cal.add(3,4));
+System.out.println("3-4="+Cal.substrat(3,4));
+System.out.println("3*4="+Cal.mul(3,4));
+System.out.println("3/4="+Cal.divide(3,4));
+
         
     }
 }
